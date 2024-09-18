5a0b5e017462261777d0085f8d357b3a2d025e9c
Goodarzi <goudarzi1212@gmail.com>
2024-09-19 01:16:14 +0330

    adding fragments

---
+++ app/src/main/java/com/fara/bookexplorer/ui/fragments/detail/DetailsFragment.kt
@@ -0,0 +1,30 @@
+package com.fara.bookexplorer.ui.fragments.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fara.bookexpolorer.R
+
+
+
+
+class DetailsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            DetailsFragment()
+    }
+}
---
+++ app/src/main/java/com/fara/bookexplorer/ui/fragments/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.fara.bookexplorer.ui.fragments.main
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.fara.bookexpolorer.R
+
+
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            MainFragment()
+    }
+}
